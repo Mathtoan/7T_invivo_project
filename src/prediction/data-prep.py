9b47f95f7c7eb424e513d8591d7ba5cd7c524fae
Mathieu Duong <mathieu.duong1@gmail.com>
2022-08-18 12:36:50 -0400

    setting up the parser

--- src/prediction/data-prep.py
+++ src/prediction/data-prep.py
@@ -7,17 +7,27 @@ from random import shuffle
 
 #%% Parser
 parser = argparse.ArgumentParser(description='Data preparation for nnUNet training.')
-parser.add_argument('-t', '--taskname', metavar='task name', type=str, required=True,
-                    help='Name of the task, should be in this format : Task[#]_[name]')
-parser.add_argument('-m', '--applymask', action='store_true',
-                    help='Apply mask to image')
-parser.add_argument('-i', '--idprefix', metavar='ID prefix', type=str,
-                    help='Set an ID prefix to file name')
+parser.add_argument('-d', '-raw_data_folder', type=str, required=True,
+                    help='Folder where the data to predict are.')
+parser.add_argument('-r', '--prediction_data_root', type=str, default='/data/mtduong/7T_invivo_project/nnUNet_prediction')
+# parser.add_argument("-i", '--input_folder', type=str,
+#                     help="Must contain all modalities for each patient in the correct"
+#                          " order (same as training). Files must be named "
+#                          "CASENAME_XXXX.nii.gz where XXXX is the modality "
+#                          "identifier (0000, 0001, etc)", required=True)
+# parser.add_argument('-o', "--output_folder", type=str, required=True,
+#                     help="folder for saving predictions")
+parser.add_argument('-t', '--task_name', type=str, required=True,
+                    help='task name or task ID, required.')
+parser.add_argument('-m', '--model', default="3d_fullres",
+                    help="2d, 3d_lowres, 3d_fullres or 3d_cascade_fullres. Default: 3d_fullres")
 
 args = parser.parse_args()
 
 #%% Parameter
-task_name = args.taskname
-applymask = args.applymask
-idprefix = args.idprefix
-
+raw_data_folder = args.raw_data_folder
+prediction_data_root = args.prediction_data_root
+# input_folder = args.input_folder
+# output_folder = args.output_folder
+task_name = args.task_name
+model = args.model
