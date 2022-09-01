7557e764690be57e0ea2ed30cf32f3687a4a77da
Mathieu Duong <mathieu.duong1@gmail.com>
2022-09-01 15:09:34 -0400

    adding brain mask

--- src/prediction/data-prep.py
+++ src/prediction/data-prep.py
@@ -19,6 +19,8 @@ parser.add_argument('-r', '--prediction_data_root', type=str, default='/data/mtd
 #                     help="folder for saving predictions")
 parser.add_argument('-t', '--task_name', type=str, required=True,
                     help='task name or task ID, required.')
+parser.add_argument('-M', '--applymask', action='store_true',
+                    help='Apply mask to image')
 parser.add_argument('-m', '--model', default="3d_fullres",
                     help="2d, 3d_lowres, 3d_fullres or 3d_cascade_fullres. Default: 3d_fullres")
 
@@ -30,6 +32,7 @@ prediction_data_root = args.prediction_data_root
 # input_folder = args.input_folder
 # output_folder = args.output_folder
 task_name = args.task_name
+apply_mask = args.applymask
 model = args.model
 
 #%% Read and write nifti functions
@@ -47,8 +50,8 @@ def save_nifti(image, filepath_name, img_obj):
 
 #%% Setting up path
 #TODO : custom mkdir so we can reset the folder
-input_folder = join(prediction_data_root, model, task_name, 'input')
-output_folder = join(prediction_data_root, model, task_name, 'output')
+input_folder = join(prediction_data_root, model, task_name, 'preprocessed/input')
+output_folder = join(prediction_data_root, model, task_name, 'preprocessed/output')
 maybe_mkdir_p(input_folder)
 maybe_mkdir_p(output_folder)
 
@@ -68,4 +71,12 @@ for i in range(len(predicted_cases)):
     input_image_file = input_image_file + "_%04.0d.nii.gz" % 0 # for now, end of file is 0000 because there is only one modality
 
     image_data, img_obj = read_nifti(raw_data_file)
-    save_nifti(image_data, input_image_file, img_obj)
+    if apply_mask:
+        # Reading the brain mask
+        brain_mask_file_name = unique_name + "_T1w_7T_Preproc_BrainMask.nii.gz"
+        brain_mask_file = join(raw_data_folder, unique_name, brain_mask_file_name)
+        brain_mask_data, brain_mask_obj = read_nifti(brain_mask_file)
+        save_nifti(np.multiply(image_data, brain_mask_data[:,:,:,0]), input_image_file, img_obj)
+    else:
+
+        save_nifti(image_data, input_image_file, img_obj)
