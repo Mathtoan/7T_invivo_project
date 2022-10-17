f3790c27374cc4ec6d2da9f59f8882bc713c15e5
Mathieu Duong <mathieu.duong1@gmail.com>
2022-10-17 17:55:36 -0400

    improvement of run_prediction_picsl.sh

--- src/prediction/run_prediction_picsl.sh
+++ src/prediction/run_prediction_picsl.sh
@@ -2,20 +2,11 @@
 root="/home/mtduong/7T_invivo_project/data/nnUNet_prediction/3d_fullres"
 operation_type=$1
 cuda=$2
+task_list="Task506_7Tgm-mergedlabels"
 
-# prediction_task_folder="${root}/Task501_7Tgm/picsl-data"
-# python3 ~/7T_invivo_project/src/prediction/data-prep-picsl-data.py -t Task501_7Tgm -o ${operation_type}
-# CUDA_VISIBLE_DEVICES=$cuda nnUNet_predict -i ${prediction_task_folder}/${operation_type}/input -o ${prediction_task_folder}/${operation_type}/output -t 504 -m 3d_fullres --disable_mixed_precision -f all
-# python3 ~/gmailsendmail/main.py -S "Task 504 Prediction on ${operation_type} done"
-
-prediction_task_folder="${root}/Task504_7Tgm-lesslabel/picsl-data"
-python3 ~/7T_invivo_project/src/prediction/data-prep-picsl-data.py -t Task504_7Tgm-lesslabel -o ${operation_type}
-CUDA_VISIBLE_DEVICES=$cuda nnUNet_predict -i ${prediction_task_folder}/${operation_type}/input -o ${prediction_task_folder}/${operation_type}/output -t 504 -m 3d_fullres --disable_mixed_precision -f all
-python3 ~/gmailsendmail/main.py -S "Task 504 Prediction on ${operation_type} done"
-
-prediction_task_folder="${root}/Task506_7Tgm-mergedlabels"
-python3 ~/7T_invivo_project/src/prediction/data-prep-picsl-data.py -t Task506_7Tgm-mergedlabels -o ${operation_type}
-CUDA_VISIBLE_DEVICES=$cuda nnUNet_predict -i ${prediction_task_folder}/${operation_type}/input -o ${prediction_task_folder}/${operation_type}/output -t 504 -m 3d_fullres --disable_mixed_precision -f all
-python3 ~/gmailsendmail/main.py -S "Task 506 Prediction on ${operation_type} done"
-
-
+for task in $task_list; do
+    prediction_task_folder="${root}/${task}/picsl-data"
+    python3 ~/7T_invivo_project/src/prediction/data-prep-picsl-data.py -t ${task} -o ${operation_type}
+    CUDA_VISIBLE_DEVICES=$cuda nnUNet_predict -i ${prediction_task_folder}/${operation_type}/input -o ${prediction_task_folder}/${operation_type}/output -t ${task} -m 3d_fullres --disable_mixed_precision -f all
+    python3 ~/gmailsendmail/main.py -S "${task} Prediction on ${operation_type} done"
+done
